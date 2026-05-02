7869dfa6b51403528cd08d977cc391a611e1d3a9
BachbeastCE <duybach2808@gmail.com>
2026-05-02 13:23:53 +0700

    #test battery

--- STM32/Debug/Core/Src/global.d
+++ STM32/Debug/Core/Src/global.d
@@ -54,7 +54,8 @@ Core/Src/global.o: ../Core/Src/global.c ../Core/Inc/global.h \
  ../Middlewares/Third_Party/FreeRTOS/Source/include/timers.h \
  ../Core/Inc/st7735.h ../Core/Inc/fonts.h ../Core/Inc/gps.h \
  ../Core/Inc/button.h ../Core/Inc/main.h ../Core/Inc/battery.h \
- ../Core/Inc/gps_kf.h ../Core/Inc/LoRa.h ../Core/Inc/micro_aes.h
+ ../Core/Inc/gps_kf.h ../Core/Inc/LoRa.h ../Core/Inc/micro_aes.h \
+ ../Core/Inc/config.h
 ../Core/Inc/global.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
@@ -121,3 +122,4 @@ Core/Src/global.o: ../Core/Src/global.c ../Core/Inc/global.h \
 ../Core/Inc/gps_kf.h:
 ../Core/Inc/LoRa.h:
 ../Core/Inc/micro_aes.h:
+../Core/Inc/config.h:
